2a8ef6a4034dfc73d691b21d9d43eac647c2175a
dane <53088317+pedersendane@users.noreply.github.com>
2022-06-22 02:17:51 +0000

    added more logging and looking into rate limiting

--- main.py
+++ main.py
@@ -4,34 +4,21 @@ import discord
 import requests
 import youtube_dl
 import utilities
+import json
+import random
 from discord.ext import commands
 from dotenv import load_dotenv
 from replit import db
 
 load_dotenv()
-# Save your discord token in a .env file and link it here.
+client = discord.Client()
 token = os.environ['discordToken']
-
-# Set the bot intents accordingly to be able to read info about guild members etc.
 intents = discord.Intents.default()
 intents.members = True
-
-bot = commands.Bot(command_prefix='$')
-
-# YouTube is a bitch and tries to disconnect our bot from its servers. Use this to reconnect instantly.
-# (Because of this disconnect/reconnect cycle, sometimes you will listen a sudden and brief stop)
+qBot = commands.Bot(command_prefix=["$"])
 FFMPEG_OPTIONS = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-
-# List with all the sessions currently active.
-# TODO: Terminate season after X minutes have passed without interaction.
 sessions = []
-
-#Create database
-db["key"] = "value"
-keys = db.keys()
-print(keys)
-
 
 def check_session(ctx):
     """
@@ -65,7 +52,7 @@ def prepare_continue_queue(ctx):
     :param ctx: discord.ext.commands.Context
     :return: None
     """
-    fut = asyncio.run_coroutine_threadsafe(continue_queue(ctx), bot.loop)
+    fut = asyncio.run_coroutine_threadsafe(continue_queue(ctx), qBot.loop)
     try:
         fut.result()
     except Exception as e:
@@ -89,7 +76,7 @@ async def continue_queue(ctx):
 
     session.q.next()
 
-    voice = discord.utils.get(bot.voice_clients, guild=session.guild)
+    voice = discord.utils.get(qBot.voice_clients, guild=session.guild)
     source = await discord.FFmpegOpusAudio.from_probe(session.q.current_music.url, **FFMPEG_OPTIONS)
 
     if voice.is_playing():
@@ -101,7 +88,7 @@ async def continue_queue(ctx):
 
 
 
-@bot.command(name='play', aliases=['add'])
+@qBot.command(name='play', aliases=['add'])
 async def play(ctx, *, arg):
     """
     Checks where the command's author is, searches for the music required, joins the same channel as the command's
@@ -132,6 +119,7 @@ async def play(ctx, *, arg):
             print(e)
             info = ydl.extract_info(f"ytsearch:{arg}", download=False)[
                 'entries'][0]
+            await ctx.send(f"No url given. Searching YouTube for '{arg}'")
         else:
             info = ydl.extract_info(arg, download=False)
 
@@ -142,10 +130,10 @@ async def play(ctx, *, arg):
     session.q.enqueue(title, url, thumb)
 
     # Finds an available voice client for the bot.
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(qBot.voice_clients, guild=ctx.guild)
     if not voice:
         await voice_channel.connect()
-        voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+        voice = discord.utils.get(qBot.voice_clients, guild=ctx.guild)
 
     # If it is already playing something, adds to the queue
     if voice.is_playing():
@@ -162,14 +150,7 @@ async def play(ctx, *, arg):
         source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
         voice.play(source, after=lambda ee: prepare_continue_queue(ctx))
 
-
-
-
-
-
-
-
-@bot.command(name='pause')
+@qBot.command(name='pause')
 async def pause(ctx):
     """
     If playing audio, pause it.
@@ -177,17 +158,13 @@ async def pause(ctx):
     :param ctx: discord.ext.commands.Context
     :return: None
     """
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(qBot.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         voice.pause()
     else:
         await ctx.send("I'm not playing anything you fucking moron")
 
-
-
-
-
-@bot.command(name='resume')
+@qBot.command(name='resume')
 async def resume(ctx):
     """
     If audio is paused, resumes playing it.
@@ -195,15 +172,13 @@ async def resume(ctx):
     :param ctx: discord.ext.commands.Context
     :return: None
     """
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(qBot.voice_clients, guild=ctx.guild)
     if voice.is_paused:
         voice.resume()
     else:
         await ctx.send("I wasn't paused dumbass")
 
-
-
-@bot.command(name='next', aliases=['skip'])
+@qBot.command(name='next', aliases=['skip'])
 async def skip(ctx):
     """
     Skips the current song, playing the next one in queue if there is one.
@@ -219,7 +194,7 @@ async def skip(ctx):
         return
 
     # Finds an available voice client for the bot.
-    voice = discord.utils.get(bot.voice_clients, guild=session.guild)
+    voice = discord.utils.get(qBot.voice_clients, guild=session.guild)
 
     # If it is playing something, stops it. This works because of the "after" argument when calling voice.play as it is
     # a recursive loop and the current song is already going to play the next song when it stops.
@@ -233,18 +208,7 @@ async def skip(ctx):
         voice.play(source, after=lambda e: prepare_continue_queue(ctx))
         return
 
-
-
-
-
-
-
-
-
-
-
-
-@bot.command(name='stop')
+@qBot.command(name='stop')
 async def stop(ctx):
     """
     Stops playing audio and clears the session's queue.
@@ -253,15 +217,14 @@ async def stop(ctx):
     :return: None
     """
     session = check_session(ctx)
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(qBot.voice_clients, guild=ctx.guild)
     if voice.is_playing:
         voice.stop()
         session.q.clear_queue()
     else:
         await ctx.send("Did you hear music playing? Didn't think so.")
 
-
-@bot.command(name='gtfo')
+@qBot.command(name='gtfo')
 async def leave(ctx):
     """
     If bot is connected to a voice channel, it leaves it.
@@ -269,15 +232,14 @@ async def leave(ctx):
     :param ctx: discord.ext.commands.Context
     :return: None
     """
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice = discord.utils.get(qBot.voice_clients, guild=ctx.guild)
     if voice.is_connected:
         check_session(ctx).q.clear_queue()
         await voice.disconnect()
     else:
         await ctx.send("Bot not connected, so it can't leave.")
       
-
-@bot.command(name='print')
+@qBot.command(name='print')
 async def print_info(ctx):
     """
     A debug command to find session id, what is current playing and what is on the queue.
@@ -295,15 +257,60 @@ async def print_info(ctx):
       await ctx.send(f"Queue: {queue}")
 
 
+@qBot.command(name='atp')
+async def add_song_to_playlist(ctx, *, arg):
+  session = check_session(ctx)
+  # Searches for the video
+  with youtube_dl.YoutubeDL({'format': 'bestaudio', 'noplaylist': 'True'}) as ydl:
+      try:
+          requests.get(arg)
+      except Exception as e:
+          print(e)
+          info = ydl.extract_info(f"ytsearch:{arg}", download=False)[
+              'entries'][0]
+          await ctx.send(f"Searching YouTube for '{arg}'")
+      else:
+          info = ydl.extract_info(arg, download=False)
+        
+  url = info['formats'][0]['url']
+  thumb = info['thumbnails'][0]['url']
+  title = info['title']
+  json_song = {"url": f"{url}", "thumb" : f"{thumb}","title" : f"{title}"}
+  db_song_key = f"playlist_item: {title}"
+  db_keys = db.keys()
+  
+  if db_song_key not in db_keys:
+    db[f"playlist_item: {title}"] = json.dumps(json_song)
+    await ctx.send(thumb)
+    await ctx.send(f"Added '{title}' to the playlist")
+    await ctx.send(f"Playlist now contains {db_keys.len()} songs")
+  else:
+    await ctx.send(f"{title} is already in the playlist")
+  print(db[f"playlist_item: {title}"])
+  
+@qBot.command(name='playlistInfo')
+async def show_playlist_info(ctx):
+  session = check_session(ctx)
+  playlist_items = db.prefix("playlist_item:")
+  await ctx.send(f"There are currently {playlist_items.len()} songs in the playlist")
+  if(playlist_items.len() > 0):
+    await ctx.send(f"Type '$shuffle' to shuffle through them or '$showPlaylist' to see all of the songs")
+  else:
+    await ctx.send(f"Type '$atp song name' to add some songs to the playlist")
+    
 
-
-
-
-
-
-
-
-
+@qBot.command(name='shuffle')
+async def shuffle_playlist(ctx):
+  session = check_session(ctx)
+  playlist_items = db.prefix("playlist_item:")
+  shuffled_items = random.shuffle(playlist_items)
+  for i in shuffled_items:
+    song = json.loads(db[f"{i}"])
+    song_title = song["title"]
+    song_url = song["url"]
+    song_thumb = song["thumb"]
+    await ctx.invoke(qBot.get_command('add'), query=f"{song_title}")
+  
 # Runs bot's loop.
+qBot.run(token)
 
-bot.run(token)
